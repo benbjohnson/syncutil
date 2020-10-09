231620451f74d4906553af5a04633d8dedb6096e
Ben Johnson <benbjohnson@yahoo.com>
2020-10-09 11:30:44 -0600

    Add additional post-action logging.

--- syncutil.go
+++ syncutil.go
@@ -20,24 +20,28 @@ type LoggingRWMutex struct {
 func (rw *LoggingRWMutex) Lock() {
 	rw.printStack("obtaining lock", debug.Stack())
 	rw.mu.Lock()
+	rw.printStack("obtained lock", debug.Stack())
 }
 
 // Unlock logs to STDERR that a lock is being released and then releases the lock.
 func (rw *LoggingRWMutex) Unlock() {
 	rw.printStack("releasing lock", debug.Stack())
 	rw.mu.Unlock()
+	rw.printStack("released lock", debug.Stack())
 }
 
 // RLock logs to STDERR that a read lock is being obtained and then obtains the read lock.
 func (rw *LoggingRWMutex) RLock() {
 	rw.printStack("obtaining read lock", debug.Stack())
 	rw.mu.RLock()
+	rw.printStack("obtained read lock", debug.Stack())
 }
 
 // RUnlock logs to STDERR that a read lock is being released and then releases the read lock.
 func (rw *LoggingRWMutex) RUnlock() {
 	rw.printStack("releasing read lock", debug.Stack())
 	rw.mu.RUnlock()
+	rw.printStack("released read lock", debug.Stack())
 }
 
 // RLocker returns the underlying mutex's locker.
